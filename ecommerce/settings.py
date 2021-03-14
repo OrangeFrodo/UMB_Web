47f17daa745155aa6d37da9a6a870a86a13cd4c2
OrangeFrodo <daxoja@gmail.com>
2021-03-14 01:51:22 +0100

    Allowed_host

--- ecommerce/settings.py
+++ ecommerce/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'z+ksf@)0d^qojbh4rnp4b1to$hq&*tt(3bs$gf(3i267g$k9ln'
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = ['192.168.1.29', '127.0.0.1', '172.20.10.6']
+ALLOWED_HOSTS = ['192.168.1.29', '127.0.0.1', '172.20.10.6', '46.101.154.215']
 
 # Emails
 
