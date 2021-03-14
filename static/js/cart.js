544dab9ea003d670cf77777a7ab96cff4a534878
OrangeFrodo <daxoja@gmail.com>
2021-03-14 23:15:09 +0100

    BFR Bands changes

--- static/js/cart.js
+++ static/js/cart.js
@@ -25,7 +25,6 @@ for (i = 0; i < updateBtns.length; i++) {
 }
 
 function updateUserOrder(productId, action){
-	console.log('User is authenticated, sending data...')
 
 		var url = '/update_item/'
 
@@ -46,7 +45,6 @@ function updateUserOrder(productId, action){
 }
 
 function addCookieItem(productId, action){
-	console.log('User is not authenticated')
 
 	if (action == 'add'){
 		if (cart[productId] == undefined){
@@ -61,17 +59,14 @@ function addCookieItem(productId, action){
 		cart[productId]['quantity'] -= 1
 
 		if (cart[productId]['quantity'] <= 0){
-			console.log('Item should be deleted')
 			delete cart[productId];
 		}
 	}
 
 	if (action == 'delete'){
-		console.log("Jou")
 		delete cart[productId];
 	}
 
-	console.log('CART:', cart)
 	document.cookie ='cart=' + JSON.stringify(cart) + ";domain=;path=/"
 	
 	location.reload()
